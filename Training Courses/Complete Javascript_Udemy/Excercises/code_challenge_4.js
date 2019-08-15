211a490cb44ef25bdd947eb679030901f71eac50
Ferguson <wesley.ferguson@aecom.com>
2019-08-15 08:21:51 +0200

    Code challenge 4 complete

--- Training Courses/Complete Javascript_Udemy/Excercises/code_challenge_4.js
+++ Training Courses/Complete Javascript_Udemy/Excercises/code_challenge_4.js
@@ -1,8 +1,8 @@
 const Mark = {
     fullName: 'Mark Rowe',
-    weight: 81,
+    weight: 86,
     height: 1.82,
-    bmi: function() {
+    calcBmi: function() {
         this.bmi = this.weight / Math.pow(this.height, 2)
         return this.bmi
     }
@@ -13,11 +13,25 @@ const John = {
     fullName: 'John Doe',
     weight: 105,
     height: 1.95,
-    bmi: function() {
+    calcBmi: function() {
         this.bmi = this.weight / Math.pow(this.height, 2)
         return this.bmi
     }
 };
 
-console.log(Mark.bmi());
-console.log(John.bmi());
+
+// Mark.calcBmi();
+// John.calcBmi();
+// console.log(Mark.bmi, John.bmi);
+
+
+if (Mark.calcBmi() > John.calcBmi()) {
+    console.log(Mark.fullName + ' is the fattest with a BMI of ' + Mark.bmi);
+}else if (Mark.bmi === John.bmi) {
+    console.log(Mark.fullName + ' and ' + John.fullName + ' are equally as fat with an average BMI of ' + ((Mark.bmi + John.bmi)/2));
+}else {
+    console.log(John.fullName + ' is the fattest with a BMI of ' + John.bmi);
+};
+
+console.log(Mark.bmi, John.bmi);
+
